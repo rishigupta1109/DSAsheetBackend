73f5cdc58700e86a106c47a1fb1e44cf9e7358bd
Rishi <grishi634@gmail.com>
2023-06-02 19:40:33 +0530

    bug fixed

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.z0nv6c1.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1rjlyay.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
